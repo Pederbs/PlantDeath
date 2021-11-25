1f3e362a5bff47a3cafa3a88cb624ffa87b09e98
Pederbs <82643433+Pederbs@users.noreply.github.com>
2021-11-25 11:33:57 +0100

    Update AlarmNode.ino
    tatt vekk sleep funksjon og la til kanpp for å skru av alarm

--- Code/AlarmNode/AlarmNode.ino
+++ Code/AlarmNode/AlarmNode.ino
@@ -1,15 +1,13 @@
 //Library
 #include "UbidotsEsp32Mqtt.h"
 
-//Defined temporary variables
-#define sleep_time 120
-#define seconds 1000000
-
 //Components 
 const int buzzerLo = 32;
 const int buzzerHi = 33;
+const int button_pin = 35;
 int critical_temp = 10;
 bool alarm_status = 0;
+bool button_status = 0;
 
 // setting PWM properties
 const int freqLo = 800;
@@ -17,12 +15,6 @@ const int freqHi = 1000;
 const int ledChannelLo = 0;
 const int ledChannelHi = 2;
 const int resolution = 1;
-
-
-unsigned long timer;
-long wait_timer = 2000;
-long alarm_duration = 300000; // 5 minutes
-
 
 //Ubidots configs
 const char *ubidots_token = "BBFF-zAB17mfcz5sGxEz17GPb5cSsyHPkRH";
@@ -37,19 +29,9 @@ Ubidots ubidots(ubidots_token);
 //Takes the data from payload and checks which sensor the payload came from. 
 void alarm(char *topic, byte *payload){
   int temp = atoi((char *)payload);
-  alarm_status = 0;
-  
   // Sets of the alarm it the temperature is under 10 degrees 
-  if(temp < critical_temp){
-    alarm_status = 1;
-    wait_timer = alarm_duration;
-
-    ledcSetup(ledChannelLo, freqLo, resolution);
-    ledcSetup(ledChannelHi, freqHi, resolution);
-    ledcAttachPin(buzzerLo, ledChannelLo);
-    ledcAttachPin(buzzerHi, ledChannelHi);
-  }
-  //return alarm_status;
+  if(temp < critical_temp){alarm_status = 1;}
+  else{alarm_status = 0;}
 }
 
 void callback(char *topic, byte *payload, unsigned int length){
@@ -68,17 +50,16 @@ void callback(char *topic, byte *payload, unsigned int length){
   Serial.println();
   //*/
 
-  // If the topic involves any word about soilsensor, then we send the data to compare the value and which sensor should get water if needed. 
-  if(strstr(topic, "temp"))
-  {
-    alarm(topic, payload);
-  }
+  // If the topic involves any word about temperature, then we send the data to compare the value amd evaluate if the alarm should be on
+  if(strstr(topic, "temp")){alarm(topic, payload);}
 }
 
 void setup() {
   //Serial.begin(115200);
-  //Put a timer on sleep mode.
-  esp_sleep_enable_timer_wakeup(sleep_time*seconds);
+  ledcSetup(ledChannelLo, freqLo, resolution);
+  ledcSetup(ledChannelHi, freqHi, resolution);
+  ledcAttachPin(buzzerLo, ledChannelLo);
+  ledcAttachPin(buzzerHi, ledChannelHi);
 
   //Wifi connection, what function should run if message arrives from subscribed variables and etc. 
   delay(10);
@@ -89,7 +70,6 @@ void setup() {
   
   // Variables for subscription. 
   ubidots.subscribeLastValue(plant_node, subscribe_temp_variable);
-  timer = millis();
 }
 
 
@@ -98,10 +78,7 @@ void loop(){
     ubidots.reconnect();
     ubidots.subscribeLastValue(plant_node, subscribe_temp_variable);
   }
-  if((millis()-timer) > wait_timer){
-    //Serial.println("    Going to sleep");
-    esp_deep_sleep_start(); 
-  }
+  if (button_status == 1){alarm_status = 0;}
   if(alarm_status == 1){
     ledcWrite(ledChannelHi, 0);
     ledcWrite(ledChannelLo, 1);
