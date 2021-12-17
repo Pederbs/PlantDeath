581d045e889193a04c57f688cb4777b979daf51d
Pederbs <pederbsa@stud.ntnu.no>
2021-12-17 15:26:11 +0100

    Litt beskrivende kommentarer
    lagt til beskrivende kommentarer, tatt vekk seriell prints, endret alarm_delay til nominal_alarm_timer

--- Code/AlarmNode/AlarmNode.ino
+++ Code/AlarmNode/AlarmNode.ino
@@ -5,12 +5,12 @@
 const int buzzerLo = 32;
 const int buzzerHi = 33;
 const int button_pin = 35;
-int critical_temp = 10;
+int critical_temp = 10; // Critical temperature value
 bool alarm_status = 0;
 bool button_status = 0;
 
 //Delays
-unsigned long alarm_delay = 300000; // alarm er av i 5 min
+unsigned long nominal_alarm_time = 300000; // 300 000 sec = 5 min
 unsigned long alarm_timer;
 
 //Setting PWM properties
@@ -18,12 +18,12 @@ const int freqLo = 800;
 const int freqHi = 1000;
 const int ledChannelLo = 0;
 const int ledChannelHi = 2;
-const int resolution = 1;
+const int resolution = 1; // Amplitude og PWM is set to resolution of 1 bit 
 
 //Ubidots configs
 const char *ubidots_token = "BBFF-zAB17mfcz5sGxEz17GPb5cSsyHPkRH";
-const char *wifi_ssid = "kameraBad2";
-const char *wifi_pass = "9D2Remember";
+const char *wifi_ssid = ""; // Insert WiFi name here
+const char *wifi_pass = ""; // Insert WiFi password here
 const char *alarm_esp32_node = "alarm";
 const char *plant_node = "demo";
 const char *subscribe_temp_variable = "temperature";
@@ -33,34 +33,20 @@ Ubidots ubidots(ubidots_token);
 //Takes the data from payload and checks which sensor the payload came from. 
 void alarm(char *topic, byte *payload){
   int temp = atoi((char *)payload);
-  // Sets of the alarm it the temperature is under 10 degrees 
+  // Activates the alarm it the temperature is less that the critical temperature 
   if(temp < critical_temp){alarm_status = 1;}
-  else{alarm_status = 0;}
+  else{alarm_status = 0;} // Deactivates the alerm if the previous if statment is not true
 }
 
 void callback(char *topic, byte *payload, unsigned int length){
   //Store the payload
   payload[length] = '\0'; // Make payload a string by NULL terminating it.
-
-  /*
-  // Print out where the message came from and what the payload has stored. 
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-  //*/
-
-  // If the topic involves any word about temperature, then we send the data to compare the value amd evaluate if the alarm should be on
+  // If the topic involves any word about temp, the data is sent to compare with critical_temp
   if(strstr(topic, "temp")){alarm(topic, payload);}
 }
 
 void setup() {
   pinMode(button_pin, INPUT);
-  //Serial.begin(115200);
   ledcSetup(ledChannelLo, freqLo, resolution);
   ledcSetup(ledChannelHi, freqHi, resolution);
   ledcAttachPin(buzzerLo, ledChannelLo);
@@ -73,27 +59,26 @@ void setup() {
   ubidots.setup();
   ubidots.reconnect();
   
-  // Variables for subscription. 
+  // Subscribing to the temp variable from Ubidots
   ubidots.subscribeLastValue(plant_node, subscribe_temp_variable);
 }
 
 
 void loop(){
-  if(!ubidots.connected()){
+  if(!ubidots.connected()){ // Reconnect to Ubidots if it is not already connected
     ubidots.reconnect();
     ubidots.subscribeLastValue(plant_node, subscribe_temp_variable);
   }
-  if (alarm_status == 1){//skjekker om knapp er påtrykket bare om alarm er aktiv
-    button_status = digitalRead(button_pin); // idle state 0
+  if (alarm_status == 1){// Checks if the button is activated only if the alarm is active
+    button_status = digitalRead(button_pin); // Idle state 0
   }
-  if (button_status == 1 && (millis() - alarm_timer) > alarm_delay){//knapp eller timer skrur av alarmen
-    //Serial.println("  Knapp paatrykket // alarm slaaes av");
+  if (button_status == 1 && (millis() - alarm_timer) > nominal_alarm_time){// Button or timer deactivated the alarm
     alarm_timer = millis();
     ledcWrite(ledChannelHi, 0);
     ledcWrite(ledChannelLo, 0);
     alarm_status = 0;
   }
-  if(alarm_status == 1){// alarm på
+  if(alarm_status == 1){// Running one cycle of the alarm
     ledcWrite(ledChannelHi, 0);
     ledcWrite(ledChannelLo, 1);
     delay(250);
@@ -102,7 +87,7 @@ void loop(){
     ledcWrite(ledChannelHi, 1);
     delay(250);
   }
-  else if(alarm_status == 0){//forsikrer om at alarm er av
+  else if(alarm_status == 0){// Ensures that the alarm is indeed off
     ledcWrite(ledChannelHi, 0);
     ledcWrite(ledChannelLo, 0);
   }
